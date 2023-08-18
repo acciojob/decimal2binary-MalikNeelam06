24cb5cc8940dd2ff37c23a1c260195498fbc0982
MalikNeelam06 <128276286+MalikNeelam06@users.noreply.github.com>
2023-08-18 10:39:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function decimalToBinary(num) {
   //Write you code here
-  
+	let unit=1;
+	let b=0;
+	while(num>0){
+	let rem = num%2;
+  num=num/2;
+		b+=rem*unit;
+		unit=unit*10;
+	}
+	consol.log(b);
 }
 
 window.decimalToBinary = decimalToBinary;
